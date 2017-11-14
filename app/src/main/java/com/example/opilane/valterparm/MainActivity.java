6586cc6bf534a5809aaad5fcbf79a4a534316e55
Transavai <valterparm@gmail.com>
2017-11-14 11:42:16 +0200

    ll

--- app/src/main/java/com/example/opilane/valterparm/MainActivity.java
+++ app/src/main/java/com/example/opilane/valterparm/MainActivity.java
@@ -1,15 +1,37 @@
-package com.example.opilane.syntaxmarioraid;
+package com.example.opilane.valterparm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.EditText;
+
+
+import com.example.opilane.valterparm.R;
 
 public class MainActivity extends AppCompatActivity {
-    TextView Tekst;
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE" ;
+    EditText Tekst;
+    Button send;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Tekst=(TextView)findViewById(R.id.Test);
+
+
+        Tekst=(EditText) findViewById(R.id.test);
+        send=(Button) findViewById(R.id.btnsend);
+
+    }
+
+    public void saada(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.text);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
